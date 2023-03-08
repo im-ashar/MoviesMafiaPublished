2cd02e92eae2d35e3e30b8ea2cb97e692f8bd28a
im-ashar <muhammadashar.401@gmail.com>
2023-03-08 17:50:04 +0500

    revert result

--- Controllers/AboutUsController.cs
+++ Controllers/AboutUsController.cs
@@ -12,31 +12,7 @@ namespace MoviesMafia.Controllers
         {
             return View();
         }
-        public async Task<IActionResult> TestYtMusicAPI()
-        {
-            var proxy = new WebProxy("144.168.217.88:8780");
-            proxy.Credentials = new NetworkCredential("ccaicvwl", "bhatti123");
-
-            var httpClientHandler = new HttpClientHandler()
-            {
-                Proxy = proxy,
-                UseProxy = true
-            };
-            var httpClient = new HttpClient(httpClientHandler);
-            YoutubeMusicSearchClient client = new YoutubeMusicSearchClient(httpClient);
-
-            var responseObject = await client.SearchAsync("krsna");
-
-            Console.WriteLine(responseObject.Results);
-
-            foreach (YoutubeVideo video in responseObject.Results)
-            {
-                Console.WriteLine(video);
-                Console.WriteLine("");
-            }
-
-            return BadRequest(responseObject.Results);
-        }
+        
     }
 }
 
